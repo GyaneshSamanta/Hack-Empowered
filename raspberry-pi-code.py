36ffdf6fa767eba912ce49a709181058502be77f
Aaishika S Bhattacharya <mail.aaishika@gmail.com>
2022-03-26 20:06:10 +0530

    ✨ Integrate Twilio

--- raspberry-pi-code.py
+++ raspberry-pi-code.py
@@ -1,11 +1,27 @@
+from twilio.rest import Client
 import RPi.GPIO as GPIO
 import time
 
+# Raspberry Pi Pins Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
+# Twilio Account Setup
+account_sid = 'AC74c73ff901fe41d63e0362f72b626798'
+auth_token = '4be8c3a8454c8294040514f876ff4c1b'
+client = Client(account_sid, auth_token)
+
+# Twilio Function
 def twilMsg(channel): 
-   print("Button pressed, send message now.")
+   print("Received input, sending message now.")
+   message = client.messages \
+    .create(
+         body='Aaishika needs help.',
+         from_='+19124612542',
+         to='+917827794110'
+     )
+
+   print(message.sid)
 
 GPIO.add_event_detect(18, GPIO.FALLING, callback = twilMsg, bouncetime = 2000)
 
